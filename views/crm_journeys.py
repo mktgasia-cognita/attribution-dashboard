21d9594c59752b8ad333a4a9bd421b4ef2527ddd
urbanonestudio <studio@urbanone.com.au>
2026-06-30 18:32:46 +0800

    Fix grade sort order to match BCS year level structure

--- views/crm_journeys.py
+++ views/crm_journeys.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 GRADE_SORT_ORDER = [
     "PreNursery", "Nursery", "Reception",
-    "Year 1", "Year 2", "Year 3", "Year 4", "Year 5", "Year 6",
-    "Year 7", "Year 8", "Year 9", "Year 10", "Year 11", "Year 12", "Year 13",
-    "PCP - Year 2", "PCP - Year 4", "PCS - Year 6",
+    "Year 1", "Year 2", "PCP - Year 2", "Year 3", "Year 4", "PCP - Year 4",
+    "Year 5", "Year 6", "Year 7", "Year 8", "Year 9",
+    "Year 10", "Year 11", "Year 12",
 ]
 
 
